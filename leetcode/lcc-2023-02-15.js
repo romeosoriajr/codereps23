6c29037aad99f56d5020ca1f7319eb2263d96254
Romeo Soria Jr <hey@romeosoriajr.com>
2023-02-15 11:03:19 -0800

    leetcode daily

---
+++ leetcode/lcc-2023-02-15.js
@@ -0,0 +1,15 @@
+function addToArrayForm(num, k) {
+  for (let i = num.length - 1; i >= 0; i--) {
+    num[i] += k;
+    k = Math.floor(num[i] / 10);
+    num[i] %= 10;
+  }
+
+  while (k > 0) {
+    num.unshift(k % 10);
+    k = Math.floor(k / 10);
+  }
+  return num;
+}
+
+
