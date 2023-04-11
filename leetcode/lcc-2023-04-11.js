264f941e940869a23523a15802696adf7d48c4a2
Romeo Soria Jr <hey@romeosoriajr.com>
2023-04-11 11:24:55 -0700

    leetcode daily

---
+++ leetcode/lcc-2023-04-11.js
@@ -0,0 +1,13 @@
+function removeStars(s) {
+  const stack = [];
+
+  for (const char of s) {
+    if (char == "*") {
+      stack.pop();
+    } else {
+      stack.push(char);
+    }
+  }
+
+  return stack.join("");
+}
